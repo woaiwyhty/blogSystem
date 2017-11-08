cf76117543e4643d5be3f5f2edc34617481b2b46
woaiwyhty <398789568@qq.com>
2017-11-08 13:16:11 -0500

    add register api

--- routes/reg.js
+++ routes/reg.js
@@ -6,11 +6,24 @@ var router = express.Router();
 var User = require('../dbModules/user');
 var valid = require('../dataProcess/validation');
 router.get('/', function(req, res, next) {
-
+    res.render('register');
 });
 
 
 router.post('/', ensureAuthenticatedInLogin, function(req, res, next) {
-    valid.register(req.body.info);
-
+    valid.register(req.body.username, req.body.password, req.body.email, req.body.birthday);
+    User.idNumberInc(function(err, doc) {
+        if(err) return next();
+        var info = {
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email,
+            birthday: new Date(req.body.birthday),
+            idNumber: doc.idNumber
+        };
+        User.addUser(info, function(err, doc) {
+            if(err) return next();
+            res.send({retCode: 0});
+        })
+    });
 });
