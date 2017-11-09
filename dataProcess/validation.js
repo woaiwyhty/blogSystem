c2970012f3cca2e6da36d14cbc2e2fe288d1bdbf
woaiwyhty <398789568@qq.com>
2017-11-09 18:54:26 -0500

    data validation

--- dataProcess/validation.js
+++ dataProcess/validation.js
@@ -2,8 +2,14 @@
  * Created by dell on 2017/11/3.
  */
 var valid = {
-    register: function(data) {
-
+    register: function(username, password, email, birthday) {
+        return true;
+    },
+    section: function(sectionName) {
+        return true;
+    },
+    thread: function(title, content, sec) {
+        return true;
     }
 };
 
