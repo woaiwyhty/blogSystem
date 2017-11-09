8ead0bbe4e3d5b55065e2c542bddd0591f659028
woaiwyhty <398789568@qq.com>
2017-11-09 14:16:44 -0500

    section management apis

---
+++ routes/section.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Wu on 11/8/2017.
+ */
+var express = require('express');
+var router = express.Router();
+var User = require('../dbModules/user');
+var valid = require('../dataProcess/validation');
+var Section = require('../dbModules/sections')
+router.get('/', function(req, res, next) {
+    // get all sections
+    Section.getAllSections(function(err, doc) {
+        if(err) return next();
+        var infoArr = [];
+        for(var i in doc) {
+            var obj = {
+                sectionName: doc[i].name,
+                threadCounts: doc[i].threadCount,
+                idNumber: doc[i].idNumber
+            };
+            infoArr.push(obj);
+        }
+
+        res.send({ retCode: 0, resArr: infoArr });
+    })
+});
+
+function ensureAdministrator(req, res, next) {
+    if(req.session.admin) {
+        return next();
+    }
+    res.status(404);
+    res.send('you cant do this');
+}
+
+router.post('/', ensureAdministrator, function(req, res, next) {
+    // add a section
+    if(!valid.section(req.body.sectionName))
+        return next();
+    Section.idNumberInc(function(err, doc) {
+        if(err) return next();
+        var info = {
+            name: req.body.sectionName,
+            threadCount: 0,
+            idNumber: doc.idNumber
+        };
+        Section.addSection(info, function(err, doc) {
+            if(err) return next();
+            res.send({ retCode: 0 });
+        });
+    })
+});
+
+module.exports = router;
