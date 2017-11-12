6e01f4b54c3fecdee210837815198e21b8bb6aec
woaiwyhty <398789568@qq.com>
2017-11-12 15:46:13 -0500

    add some indexes

--- dbManipulations/dbHandles.js
+++ dbManipulations/dbHandles.js
@@ -9,21 +9,21 @@ module.exports = {
         password: {type: String, required: true},
         email: {type: String},
         birthday: {type: Date},
-        idNumber: {type: Number, unique: true}
+        idNumber: {type: Number, unique: true, index: 1}
     },
     sections: {
-        name: {type: String, required: true, index: 1, unique: true},
+        name: {type: String, required: true, unique: true},
         threadCount: {type: Number},
-        idNumber: {type: Number, unique: true}
+        idNumber: {type: Number, unique: true, index: 1}
     },
     threads: {
         threadTitle: {type: String},
         threadContent: {type: String},
         threadDate: {type: Date},
-        belongUserId: {type: Schema.Types.ObjectId, ref: 'users'},
-        belongSectionId: {type: Schema.Types.ObjectId, ref: 'sections'},
+        belongUserId: {type: Schema.Types.ObjectId, ref: 'users', index: 1},
+        belongSectionId: {type: Schema.Types.ObjectId, ref: 'sections', index: 1},
         userName: {type: String},
-        idNumber: {type: Number, unique: true}
+        idNumber: {type: Number, unique: true, index: 1}
     },
     ids: {
         name: {type: String, required: true, unique: true},
