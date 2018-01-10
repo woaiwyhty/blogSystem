7272f23144f302d9342283d920c0f9e998836a99
Yuhan Wu <398789568@qq.com>
2018-01-10 15:20:37 -0500

    validate the input data

--- dataProcess/validation.js
+++ dataProcess/validation.js
@@ -1,14 +1,27 @@
 /**
  * Created by dell on 2017/11/3.
  */
+var validModule = require('validator');
 var valid = {
     register: function(username, password, email, birthday) {
+        if(typeof username !== 'string' && username.length < 8)
+            return false;
+        if(typeof password !== 'string' && password.length < 6)
+            return false;
+        if(!validModule.isEmail(email))
+            return false;
         return true;
     },
     section: function(sectionName) {
+        if(typeof sectionName !== 'string' && validModule.isEmpty(sectionName))
+            return false;
         return true;
     },
     thread: function(title, content, sec) {
+        if(typeof title !== 'string' && validModule.isEmpty(title))
+            return false;
+        if(typeof content !== 'string' && validModule.isEmpty(content))
+            return false;
         return true;
     }
 };
